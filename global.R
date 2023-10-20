0317aaf5584c8eeea6d3be45b90e52f190d50a86
FOSTER <Charlotte.FOSTER@education.gov.uk>
2023-10-20 11:23:36 +0100

    add git2r package to library

--- global.R
+++ global.R
@@ -9,6 +9,7 @@
 # renv::status()
 # Library calls ---------------------------------------------------------------------------------
 shhh <- suppressPackageStartupMessages # It's a library, so shhh!
+shhh(library(git2r, pos = 10000))
 shhh(library(ggplot2))
 shhh(library(shinydashboard))
 shhh(library(shinyWidgets))
